af475769728e981b6ece9aa1908a1e73eb3c4d60
Eli Serricchio <eliserricchio@gmail.com>
2022-01-22 19:29:08 -0300

    usando templates

--- AppCoder/views.py
+++ AppCoder/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from AppCoder.models import Curso
+  
 
 def crear_curso(request, camada):
     curso = Curso (nombre = 'Python', camada = camada)
     curso.save()
     return HttpResponse(f'Curso Creado! {camada}')  
+
+def inicio (request):
+    return render(request, 'AppCoder/inicio.html') 
+    
+
+def cursos (request):
+    return render(request, 'AppCoder/cursos.html')  
+
+def profesores (request):
+    return HttpResponse('Profesores')
+
+def estudiantes (request):
+    return HttpResponse('Estudiantes')
+
+def entregables (request):
+    return HttpResponse('Entregables')
